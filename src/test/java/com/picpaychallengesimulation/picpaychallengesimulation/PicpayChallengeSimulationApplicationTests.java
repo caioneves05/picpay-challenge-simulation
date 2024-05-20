495f30357ad4862c48283e0ba25544394f6f1661
Caio Neves <ncaio037@gmail.com>
2024-05-19 23:05:20 -0300

    feat: user entity

---
+++ src/test/java/com/picpaychallengesimulation/picpaychallengesimulation/PicpayChallengeSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaychallengesimulation.picpaychallengesimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayChallengeSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
